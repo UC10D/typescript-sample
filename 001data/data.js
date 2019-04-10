197ec2e486c4a66431696e40f21db69b8c561411
822ll <2851729134@qq.com>
2019-04-10 09:15:39 +0800

    ts

---
+++ 001data/data.js
@@ -0,0 +1,8 @@
+// typeof()
+// console.log()
+var a = null;
+console.log(typeof (a));
+console.log(a);
+a = 123;
+console.log(typeof (a));
+console.log(a);
---
+++ 001data/data.ts
@@ -0,0 +1,11 @@
+// typeof()
+// console.log()
+
+let a: number = null;
+console.log(typeof (a))
+console.log(a)
+
+a = 123;
+
+console.log(typeof (a))
+console.log(a)
