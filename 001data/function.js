3f559124122d21f72ae443cd47c3961fc706182c
ehom <liu.ehom@gmail.com>
2019-04-11 22:17:46 +0800

    feat: commit code

--- 001data/function.js
+++ 001data/function.js
@@ -1,21 +1,28 @@
-// 基础函数
+console.log("----------------基础函数--------------------------");
 function add(x, y) {
     return x + y;
 }
 var myadd = function (x, y) { return x + y; };
-console.log("function1 is: " + add(1, 2));
-console.log("function2 is " + myadd);
-// ----------------------------------------
-// 可选参数和默认参数
-var str = "bob";
+console.log("basic function is: " + add(1, 2));
+console.log("anonymous function is " + myadd);
+console.log("anonymous function is " + myadd(2, 3));
+console.log("----------------可选参数和默认参数-----------------");
+var str1 = "bob";
 var str2 = "wang";
 function buildName(firstName, lastName) {
-    if (lastName === void 0) { lastName = "smith"; }
+    console.log("lastName is " + lastName);
+    console.log("type is " + typeof (lastName));
     return firstName + " and " + lastName;
 }
-console.log(buildName(str, str2));
-// ----------------------------------------
-//剩余参数
+console.log(buildName(str1));
+// 可选参数为空时, 在函数里的值是 undefined
+function buildName2(firstName, lastName) {
+    if (firstName === void 0) { firstName = "wang"; }
+    return firstName + " and " + lastName;
+}
+console.log("buildName2 is " + buildName2(undefined, str2));
+// 当第一个参数有默认值时, 第一个必须传入undefined才能激活默认值
+console.log("----------------剩余参数-------------------------");
 function saveName(firstName) {
     var restOfName = [];
     for (var _i = 1; _i < arguments.length; _i++) {
@@ -23,7 +30,5 @@ function saveName(firstName) {
     }
     return firstName + " " + restOfName.join(" ");
 }
-var employeeName = saveName("Joseph", "2", 2);
+var employeeName = saveName("Joseph", "Samuel", "Lucas", "MacKinzie");
 console.log(saveName(employeeName));
-// this和箭头函数
-alert("aaa");
--- 001data/function.ts
+++ 001data/function.ts
@@ -1,30 +1,37 @@
-// 基础函数
+console.log("----------------基础函数--------------------------");
 function add(x: number, y:number): number {
     return x + y;
 }
-
 let myadd = function(x,y) {return x + y;};
 
-console.log("function1 is: " + add(1,2));
-console.log("function2 is " + myadd);
+console.log("basic function is: " + add(1,2));
+console.log("anonymous function is " + myadd);
+console.log("anonymous function is " + myadd(2,3));
 
-// ----------------------------------------
 
-// 可选参数和默认参数
-let str: string = "bob";
+console.log("----------------可选参数和默认参数-----------------");
+let str1: string = "bob";
 let str2: string = "wang";
-function buildName(firstName: string, lastName = "smith") {
+
+function buildName(firstName: string, lastName? : string) {
+    console.log("lastName is " + lastName);
+    console.log("type is " + typeof(lastName));
     return firstName + " and " + lastName;
 }
+console.log(buildName(str1));
+// 可选参数为空时, 在函数里的值是 undefined
 
-console.log(buildName(str,str2));
+function buildName2(firstName = "wang", lastName : string) {
+    return firstName + " and " + lastName;
+}
+console.log("buildName2 is " + buildName2(undefined,str2));
+// 当第一个参数有默认值时, 第一个必须传入undefined才能激活默认值
 
-// ----------------------------------------
 
-//剩余参数
-function saveName(firstName: string, ...restOfName: any[]) {
+console.log("----------------剩余参数-------------------------");
+function saveName(firstName: string, ...restOfName: string[]) {
     return firstName + " " + restOfName.join(" ");
   }
   
-  let employeeName = saveName("Joseph", "2", 2);
+  let employeeName = saveName("Joseph", "Samuel", "Lucas", "MacKinzie");
   console.log(saveName(employeeName));
