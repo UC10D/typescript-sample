8875cef5524dbd19901f6350afca5967eb6dc4db
ehom <liu.ehom@gmail.com>
2019-04-12 19:15:45 +0800

    feat: commit 'Class' code

--- 001data/class.ts
+++ 001data/class.ts
@@ -19,6 +19,17 @@ console.log(greeter.greet());
 console.log("-------------------------继承------------------------");
 class Animal {
     move(distanceInMeters: number = 0) {
-
+        console.log('Animal moved ${distanceInMeters}m.');
     }
 }
+
+class Dog extends Animal {
+    bark(): void {
+        console.log("Woof! woof!");
+    }
+}
+
+const dog = new Dog();
+console.log(dog.move(10));
+console.log(dog.bark);
+console.log();
