5bab2d02468388aa0f7f2b554e9ca38b53992d67
ehom <841013169@qq.com>
2019-05-18 16:05:31 +0800

    feat: 添加可选接口代码

--- 001data/interface.ts
+++ 001data/interface.ts
@@ -2,6 +2,9 @@
 
 console.log('--------接口的初探--------');
 
+/* LabelledValue 好比接口的名字, 用来描述上面例子里的要求.
+只要传入的对象满足上面的必要条件 有一个叫label并且类型为string的属性. 他就是被允许的
+*/
 interface LabelledValue {
     label: string;
 }
@@ -12,4 +15,26 @@ function printLabel (labelledObj: LabelledValue) {
 
 let myObj = {size: 10, label:'Size 10 object'};
 
-console.log(printLabel(myObj));
+printLabel(myObj);
+
+console.log('--------可选属性--------');
+interface SquareConfig {
+    // 定义一个接口, 其中有两个可选参数 color和width
+    color?: string;
+    width?: number;
+}
+
+function createSquare(config: SquareConfig) : {color: string; area: number} {
+    let newSquare = {color: 'white', area: 100};
+    if (config.color) {
+        newSquare.color = config.color;
+        console.log(newSquare.color);
+    }
+    if (config.width) {
+        newSquare.area = config.width * config.width;
+        console.log(newSquare.area);
+    }
+    return newSquare;
+}
+
+let mySquare = createSquare({color: "black", width: 2});
--- 001data/js/interface.js
+++ 001data/js/interface.js
@@ -5,4 +5,18 @@ function printLabel(labelledObj) {
     console.log(labelledObj.label);
 }
 var myObj = { size: 10, label: 'Size 10 object' };
-console.log(printLabel(myObj));
+printLabel(myObj);
+console.log('--------可选属性--------');
+function createSquare(config) {
+    var newSquare = { color: 'white', area: 100 };
+    if (config.color) {
+        newSquare.color = config.color;
+        console.log(newSquare.color);
+    }
+    if (config.width) {
+        newSquare.area = config.width * config.width;
+        console.log(newSquare.area);
+    }
+    return newSquare;
+}
+var mySquare = createSquare({ color: "black", width: 2 });
